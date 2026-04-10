8cda4630afcde79f29ce83cb73d1dc58071688fd
danko beribak <beribakdanko@gmail.com>
2026-04-10 22:18:39 +0200

    docker image file add node

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,33 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :set_locale
+  before_action :set_theme
+
+  helper_method :current_theme, :dark_theme?
 
   private
 
   def set_locale
     I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
     session[:locale] = I18n.locale
+  end
+
+  def set_theme
+    selected_theme = params[:theme].presence_in(%w[dark light]) || cookies[:theme].presence_in(%w[dark light]) || "dark"
+    cookies[:theme] = {
+      value: selected_theme,
+      expires: 1.year.from_now,
+      same_site: :lax
+    }
+    @current_theme = selected_theme
+  end
+
+  def current_theme
+    @current_theme || "dark"
+  end
+
+  def dark_theme?
+    current_theme == "dark"
   end
 
   def default_url_options
